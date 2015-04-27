c551be77294eee1991754956a3df36860218f2e9
alexandrehmelo <alexandrehmelo@gmail.com>
2015-04-27 11:39:01 -0300

    teste

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -10,8 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-        bugrao eo
+        <?php echo 'bugrao eo';
         ?>
     </body>
 </html>
